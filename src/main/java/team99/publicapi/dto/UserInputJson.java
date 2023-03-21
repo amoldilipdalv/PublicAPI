6cb00caaaa34e32f0f5332f156c44aba28ae794e
Dalvi Amol Dilip  (NCS) <dilip.dalvi.amol@ncs.com.sg>
2023-03-21 18:52:45 +0800

    added save user

---
+++ src/main/java/team99/publicapi/dto/UserInputJson.java
@@ -0,0 +1,14 @@
+package team99.publicapi.dto;
+
+public class UserInputJson {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
